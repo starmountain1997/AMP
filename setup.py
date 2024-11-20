c137990f991851696e7111a2d58a3d0c7dee6a16
guozr <guozr1997@hotmail.com>
2024-11-20 09:59:16 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,8 @@ setup(
     author="guozr",                        # Author's name
     author_email="guozr1997@hotmail.com",     # Author's email
     url="https://github.com/starmountain1997/AMP",# URL for your project (e.g., GitHub)
-    packages=find_packages(),                  # Automatically find packages
+    packages=find_packages(include=["amp","amp.*"]),
     install_requires=[                         # Dependencies
-        "numpy<2",
-        "transformers",
-        "torch",
-        "torch_npu",
     ],
     classifiers=[                              # Classifiers for PyPI
         "Programming Language :: Python :: 3",
@@ -22,8 +18,4 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',                   # Minimum Python version
-    include_package_data=True,                 # Include non-code files specified in MANIFEST.in
-    package_data={                             # Include specific package data
-        "AMP": ["models/*.py"],
-    },
 )
