c9790ab7fe5856bf13eee9b34f5a01f7975c7257
starmountain1997 <77533802+starmountain1997@users.noreply.github.com>
2024-11-21 20:29:25 +0800

    Update deepspeed.py

--- amp/models/deepspeed.py
+++ amp/models/deepspeed.py
@@ -2,7 +2,7 @@ import os
 
 import deepspeed
 import torch
-from deepspeed.ops.op_builder import (WARNING, cuda_minor_mismatch_ok,
+from deepspeed.ops.op_builder.builder import (WARNING, cuda_minor_mismatch_ok,
                                       installed_cuda_version)
 
 from ..module_patcher import when_imported
