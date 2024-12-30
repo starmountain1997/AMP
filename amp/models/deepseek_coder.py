e9cc665158b0d4549394cd1388796435b3d1b3f4
guozr <guozr1997@hotmail.com>
2024-12-30 19:30:12 +0800

    deepseek coder更新

--- amp/models/deepseek_coder.py
+++ amp/models/deepseek_coder.py
@@ -1,6 +1,3 @@
-# Yi系列优化
-
-
 from loguru import logger
 
 from ..module_patcher import when_imported
@@ -8,7 +5,14 @@ from .llama import llama_rms_norm_forward, npu_apply_rotary_pos_emb
 
 
 @when_imported("transformers")
-def patch_yi(mod):
-    logger.info("patched yi series")
+def patch_deepseek_coder(mod):
+    """
+    已在 deepseek-coder-6.7b-instruct       模型上测试通过
+         deepseek-coder-33b-instruct
+         deepseek-coder-6.7b-base
+         deepseek-coder-7b-instruct-v1.5
+         
+    """
+    logger.info("patched deepseek_coder")
     mod.models.llama.modeling_llama.LlamaRMSNorm.forward = llama_rms_norm_forward
     mod.models.llama.modeling_llama.apply_rotary_pos_emb = npu_apply_rotary_pos_emb
