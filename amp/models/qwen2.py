0e2e4ca7e24c32dd4c4dbee3ddb1752d6dc4d017
starmountain1997 <77533802+starmountain1997@users.noreply.github.com>
2025-01-06 17:06:17 +0800

    Update qwen2.py

--- amp/models/qwen2.py
+++ amp/models/qwen2.py
@@ -13,5 +13,5 @@ def patch_qwen2(mod):
 @when_imported("transformers")
 def patch_qwen2_moe(mod):
     logger.info("patched qwen2_moe")
-    mod.models.qwen2_moe.modeling_qwen2_moe.Qwen2RMSNorm.forward = llama_rms_norm_forward
+    mod.models.qwen2_moe.modeling_qwen2_moe.Qwen2MoeRMSNorm.forward = llama_rms_norm_forward
     mod.models.qwen2_moe.modeling_qwen2_moe.apply_rotary_pos_emb = npu_apply_rotary_pos_emb 
